157dd0a628677c57e3f84e22e75d5a3f8ac660e9
Eduardo Dourado <ed320819@gmail.com>
2025-11-22 11:06:44 -0300

    code cleanup and minor fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 const siteConfig = {
   title: "Eduardo Dourado | Full-Stack",
-  description: "Backend-focused Full-Stack Developer specializing in building robust and scalable APIs with Python (Django) and Node.js. Experienced in microservices architecture, PostgreSQL data modeling, and complex transactional systems. Seeking to leverage my skills to create high-impact solutions that merge efficiency, security, and user experience.",
+  description: "Backend-focused Full-Stack Developer seeking to leverage my skills to create high-impact solutions that merge efficiency, security, and user experience.",
   url: "https://efdourado.vercel.app",
 };
 
